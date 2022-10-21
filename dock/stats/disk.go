7dfde7650a397958fbaf4e95fb2d4a593ed4a4f3
Theo Selpin <theo@Theos-MacBook-Air.local>
2022-10-21 10:41:29 +0200

    addded Disk stats type with read+write calculation

--- dock/stats/disk.go
+++ dock/stats/disk.go
@@ -7,6 +7,23 @@ type Disk struct {
 	Write float64
 }
 
-func NewDisk(disk types.BlkioStats) {
+func NewDisk(disk types.BlkioStats) *Disk {
+	var read, write uint64
+	for _, io := range disk.IoServiceBytesRecursive {
+		if len(io.Op) > 0 {
+			switch io.Op[0] {
+			case 'r', 'R':
+				read = read + io.Value
+			case 'w', 'W':
+				write = write + io.Value
+			}
+		} else {
+			continue
+		}
+	}
 
+	return &Disk{
+		Read:  float64(read),
+		Write: float64(write),
+	}
 }
