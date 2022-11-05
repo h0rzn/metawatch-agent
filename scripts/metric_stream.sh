d9de592407cdeeb096321ecf8e5bd090589bee4f
h0rzn <hrzn@protonmail.com>
2022-11-05 13:33:33 +0100

    removed dev scripts from master

--- scripts/metric_stream.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-CONTAINER=$1
-
-websocat --exit-on-eof ws://localhost:8080/containers/$CONTAINER/stream
--- scripts/stats_for_container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-URL=http:/localhost/containers/$1/stats?stream=false
-
-curl --unix-socket /var/run/docker.sock $URL | python3 -m json.tool
