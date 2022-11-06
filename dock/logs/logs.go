8e44624942637c3a0bf85d9b5fd430290fad6d2d
h0rzn <hrzn@protonmail.com>
2022-11-06 13:03:27 +0100

    improved log parsing/gathering logic

--- dock/logs/logs.go
+++ dock/logs/logs.go
@@ -1,54 +1,97 @@
 package logs
 
-import "sync"
+import (
+	"encoding/binary"
+	"io"
+	"strings"
+)
 
-// add logic to "ignore" if no consumers are registered
-// as the logs are not supposed to be stored
-// maybe request logs from docker engine on demand?
-type LogStreamer struct {
-	mutex     *sync.Mutex
-	Consumers map[*Consumer]bool
-	Reg       chan *Consumer
-	Ureg      chan *Consumer
-	Dis       chan *Entry
+type Logs struct {
+	Reader struct {
+		Active bool
+		R      io.Reader
+	}
+	Streamer Streamer
+
+	// "active" / "inactive"
+	// "refreshing" indicates that refreshing is in action and no
+	// new refreshes should be called
+	State   string
+	Refresh chan io.Reader
+}
+
+type Streamer struct {
+	Reg  chan Consumer
+	Ureg chan Consumer
+	Dis  chan *Entry
+	Done chan bool
 }
 
 type Consumer struct {
+	Get  chan Entry
+	Done chan bool
 }
 
-func NewLogStreamer() *LogStreamer {
-	return &LogStreamer{
-		mutex:     &sync.Mutex{},
-		Consumers: make(map[*Consumer]bool),
-		Reg:       make(chan *Consumer),
-		Ureg:      make(chan *Consumer),
-		Dis:       make(chan *Entry),
+type Entry struct {
+	Time string `json:"when"`
+	Type string `json:"type"`
+	Data string `json:"data"`
+}
+
+func (s *Streamer) Request()
+
+func (s *Streamer) Parse(d chan bool, r io.Reader) (<-chan Entry, <-chan error) {
+	out := make(chan Entry)
+	errChan := make(chan error)
+
+	hdr := make([]byte, 8)
+	for {
+		select {
+		case <-d:
+			close(out)
+			close(errChan)
+		default:
+		}
+
+		sizes := binary.BigEndian.Uint32(hdr[:4])
+		content := make([]byte, sizes)
+		_, err := r.Read(content)
+		if err != nil && err != io.EOF {
+			errChan <- err
+		}
+
+		time, data, found := strings.Cut(string(content), " ")
+		if found {
+			var streamType string
+			if hdr[0] == 1 {
+				streamType = "stdout"
+			} else {
+				streamType = "stderr"
+			}
+
+			out <- Entry{
+				Time: time,
+				Type: streamType,
+				Data: data,
+			}
+		}
 	}
 }
 
-func (s *LogStreamer) Run() {
-	// set pipeline
+func (s *Streamer) Run() {
 
-	go func() {
+	go func(done chan bool) {
+		// distribute
+	}(s.Done)
+
+	for {
 		select {
 		case c := <-s.Reg:
-			s.mutex.Lock()
-			s.Consumers[c] = true
-			s.mutex.Unlock()
+			_ = c
 		case c := <-s.Ureg:
-			s.mutex.Lock()
-			// close input channel of consumer
-			delete(s.Consumers, c)
-			s.mutex.Unlock()
-		case entry := <-s.Dis:
-			for consumer := range s.Consumers {
-				_, _ = consumer, entry
-				// send data to consumer
-			}
-			// add die/done channel handling
+			_ = c
+		case <-s.Done:
+			// handle shutdown of streamer
 		}
-	}()
-
-	// distribute entries
-
+	}
 }
