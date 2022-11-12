252c889224c0f60a1f25ce7f881a1eef1f8a54f9
h0rzn <hrzn@protonmail.com>
2022-11-12 13:11:38 +0100

    cleaned up ws endpoints

--- api/ws/message.go
+++ api/ws/message.go
@@ -1,16 +1,13 @@
 package ws
 
-import "encoding/json"
-
 type Message struct {
 	Type string      `json:"type"`
 	Data interface{} `json:"data"`
 }
 
-func NewMessage(setType string, data interface{}) ([]byte, error) {
-	msg := &Message{
+func NewMessage(setType string, data interface{}) *Message {
+	return &Message{
 		Type: setType,
 		Data: data,
 	}
-	return json.Marshal(msg)
 }
