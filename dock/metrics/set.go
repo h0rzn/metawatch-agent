04b66b4cf875614c2617f938cffdc193444cdba8
h0rzn <hrzn@protonmail.com>
2023-01-23 21:54:55 +0100

    improved /container/:id/metrics endpoint (average)

--- dock/metrics/set.go
+++ dock/metrics/set.go
@@ -33,3 +33,55 @@ func NewSetWithJSON(stats types.StatsJSON) Set {
 		Net:  *NewNet(stats.Networks),
 	}
 }
+
+func Average(metrics []Set) Set {
+	var result Set
+	c := len(metrics)
+	cfloat := float64(c)
+
+	for _, set := range metrics {
+		result.CPU.UsagePerc += set.CPU.UsagePerc
+		result.CPU.Online += set.CPU.Online
+
+		result.Mem.Usage += set.Mem.Usage
+		result.Mem.UsagePerc += set.Mem.UsagePerc
+		result.Mem.Available += set.Mem.Available
+
+		result.Disk.Read += set.Disk.Read
+		result.Disk.Write += set.Disk.Write
+
+		result.Net.In += set.Net.In
+		result.Net.Out += set.Net.Out
+	}
+
+	result.When = metrics[len(metrics)-1].When
+
+	result.CPU.UsagePerc = result.CPU.UsagePerc / cfloat
+	result.CPU.Online = result.CPU.Online / cfloat
+
+	result.Mem.Usage = result.Mem.Usage / cfloat
+	result.Mem.UsagePerc = result.Mem.UsagePerc / cfloat
+	result.Mem.Available = result.Mem.Available / cfloat
+
+	result.Disk.Read = result.Disk.Read / cfloat
+	result.Disk.Read = result.Disk.Read / cfloat
+
+	result.Net.In = result.Net.In / cfloat
+	result.Net.Out = result.Net.Out / cfloat
+
+	return result
+}
+
+func Chunk(metrics []Set, chunkSize int) [][]Set {
+	var chunks [][]Set
+	for i := 0; i < len(metrics); i += chunkSize {
+		end := i + chunkSize
+
+		if end > len(metrics) {
+			end = len(metrics)
+		}
+
+		chunks = append(chunks, metrics[i:end])
+	}
+	return chunks
+}
